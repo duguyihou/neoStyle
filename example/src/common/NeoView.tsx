438e0dae5ef1aa1eef0fb75bdfc0a1f0cb8da92f
kong <duguyihou@gmail.com>
2023-06-29 20:26:39 +1000

    build: esnext

--- example/src/common/NeoView.tsx
+++ example/src/common/NeoView.tsx
@@ -1,14 +1,27 @@
 import {View, StyleProp, ViewStyle} from 'react-native';
 import React, {PropsWithChildren} from 'react';
-import {bg, BgProps, compose, layout, LayoutProps, useStyle} from 'neostyle';
+import {
+  bg,
+  BgProps,
+  border,
+  BorderProps,
+  compose,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+  useStyle,
+} from 'neostyle';
 import {Theme} from '../theme';
 
 type StyleProps = {
   style?: StyleProp<ViewStyle>;
 } & BgProps<Theme> &
-  LayoutProps<Theme>;
+  LayoutProps<Theme> &
+  SpacingProps<Theme> &
+  BorderProps<Theme>;
 
-const styleParser = compose<Theme, StyleProps>([bg, layout]);
+const styleParser = compose<Theme, StyleProps>([bg, layout, spacing, border]);
 
 type Props = PropsWithChildren<StyleProps>;
 
