7455a57d946deba144128db50c184f78957b1fce
kong <duguyihou@gmail.com>
2023-06-29 19:45:03 +1000

    refactor: example

--- example/src/Home.tsx
+++ example/src/Home.tsx
@@ -3,15 +3,24 @@ import React from 'react';
 import NeoView from './common/NeoView';
 
 const Home = () => {
-  const viewSize = {width: 100, height: 100};
   return (
     <View style={styles.container}>
       <Text>Home</Text>
       <NeoView
         bg={{sm: 'primaryBg', md: 'secondaryBg', lg: 'tertiaryBg'}}
-        style={viewSize}
+        width={{sm: 100, md: 150, lg: 200}}
+        height={{sm: 100, md: 150, lg: 200}}
       />
-      <NeoView bg={'secondaryBg'} style={viewSize} />
+      <NeoView
+        bg={{sm: 'secondaryBg', md: 'tertiaryBg', lg: 'primaryBg'}}
+        width={{sm: 100, md: 150, lg: 200}}
+        height={{sm: 100, md: 150, lg: 200}}
+      />
+      <NeoView
+        bg={{sm: 'tertiaryBg', md: 'primaryBg', lg: 'secondaryBg'}}
+        width={{sm: 100, md: 150, lg: 200}}
+        height={{sm: 100, md: 150, lg: 200}}
+      />
     </View>
   );
 };
